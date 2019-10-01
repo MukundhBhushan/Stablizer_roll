4f24d78b928a3a08db9b5220a60e11d31a69fe71
Gautamjhawar <jhawargautam@gmail.com>
2019-10-01 16:05:03 +0530

    init commit

---
+++ motor_tiny85.ino
@@ -0,0 +1,20 @@
+#define d 4                           // enable for motor control
+#define pwm 1
+
+
+void setup() {
+
+  
+}
+
+void loop() {
+       digitalWrite(d,LOW);
+      digitalWrite(pwm,HIGH);
+      delay(2000);
+      digitalWrite(d,LOW);
+      digitalWrite(pwm,LOW);
+      delay(2000);
+      digitalWrite(d,HIGH);
+      digitalWrite(pwm,HIGH);
+      delay(2000);
+}
